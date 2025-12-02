8c5ff432e81ff32cd6ec7b8794f3d0b4e7b0b9ec
Fabio <fabiio.fiuza@gmail.com>
2025-12-02 11:22:11 -0300

    complete selection sort implementation

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,41 @@
+from time import perf_counter_ns
+
+def selection_sort(arr: list[int]) -> list[int]:
+    """
+    Sorts a list of integers using the selection sort algorithm.
+    
+    Parameters:
+    - arr: List of integers to be sorted.
+    
+    Returns:
+    - The sorted list of integers.
+    """
+    
+    arr_size = len(arr)
+    
+    for i in range(arr_size):
+        min_index = i
+        
+        for j in range(i + 1, arr_size):
+            
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
+
+if __name__ == "__main__":
+    
+    worst_case_unsorted_list = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+    
+    unsorted_list = worst_case_unsorted_list.copy()
+    
+    start_time = perf_counter_ns()
+    sorted_list = selection_sort(unsorted_list)
+    end_time = perf_counter_ns()
+    
+    selection_sort_time = end_time - start_time
+    
+    print(f"Selection Sort Time: {selection_sort_time} ns")
